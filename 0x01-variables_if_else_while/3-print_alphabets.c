20e6704f32c489a030a758e6e672b9a50b82cd17
MathatoSeliane <mathatos.seliane@gmail.com>
2023-04-21 07:48:00 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-	char lcs[26] = 'abcdefghijklmnopqrstuvwxyz';
+	char alp[26] = 'abcdefghijklmnopqrstuvwxyz';
 	int i;
 
 	for (i = '0'; i < '26'; i++)
 	{
-		putchar(lcs[i]);
+		putchar(alp[i]);
 	}
 	putchar ('\n');
 	return (0);
