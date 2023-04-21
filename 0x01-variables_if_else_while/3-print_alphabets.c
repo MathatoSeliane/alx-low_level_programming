b9935da5aa0e95b0bb08491ac86934634d538844
MathatoSeliane <mathatos.seliane@gmail.com>
2023-04-21 07:30:13 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet in lower and uppercase.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char lcs;
+
+	for (lcs = 'a'; lcs <= 'z'; lcs++)
+	{
+		putchar(lcs);
+	}
+	putchar ('\n");
+	return (0);
+}
