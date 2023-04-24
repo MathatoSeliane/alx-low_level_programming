7ea171588e1bef32b8004ea2c8f5dbcfa586cdb7
MathatoSeliane <mathatos.seliane@gmail.com>
2023-04-24 04:02:01 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print lowercase in reverse.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char al:
+
+		for (al = 'z'; al >= 'a'; al--)
+			putchar(al);
+
+	putchar('\n');
+	return (0);
+}
