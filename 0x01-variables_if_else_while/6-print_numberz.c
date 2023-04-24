5957794a1869fa7f458ba53770b6efe75c842ea4
MathatoSeliane <mathatos.seliane@gmail.com>
2023-04-24 03:54:55 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print number zero to nine without using.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
