a18c92f7f25a256d68a39ca5757971415f7d20b4
MathatoSeliane <mathatos.seliane@gmail.com>
2023-04-24 06:41:19 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,14 @@
 int main(void)
 {
 	int a;
-	
-	for (a = 48; a < 58; a++)
 	{
+	for (a = 48; a < 58; a++)
 		putchar(a);
 		if (a != 57)
-		{
+
 			putchar(',');
 			putchar(' ');
-		}
+
 	putchar('\n');
 	return (0);
 }
