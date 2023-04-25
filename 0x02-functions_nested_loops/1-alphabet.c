edc80f399b0107ab9b69c2761489a1f6ffc43850
MathatoSeliane <mathatos.seliane@gmail.com>
2023-04-25 10:37:55 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Print alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+
+		_putchar(al);
+
+	_putchar('\n');
+}
+
