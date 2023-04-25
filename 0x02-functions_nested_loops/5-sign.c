97c82ccbbe2fb251b5fbd2942316f912c2ea70eb
MathatoSeliane <mathatos.seliane@gmail.com>
2023-04-25 12:24:11 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The input number as an integer
+ * Return: 1 is graeter than zero or 0 is zero
+ * or -1 is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
